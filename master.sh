e1c7fc322626908363699f0a70e5abf199aa7638
Yousuf Ali <yousuf.pharma@gmail.com>
2023-12-06 10:31:49 -0500

    fzf

--- master.sh
+++ master.sh
@@ -1,4 +1,9 @@
+sh <(curl -L https://nixos.org/nix/install) --daemon --yes
+source /etc/profile
+echo "source updated"
+
 nix-env -iA vim -f https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz
+
 nix-env -iA tmux -f https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz
 nix-env -iA nodejs_20 -f https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz
 nix-env -iA python310Packages.python -f https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz
@@ -7,6 +12,9 @@ nix-env -iA nixpkgs.rPackages.xml2
 nix-env -iA nixpkgs.rPackages.languageserver
 nix-env -iA nixpkgs.python310Packages.radian
 
+nix-env -iA nixpkgs.fzf
+nix-env -iA nixpkgs.fd
+nix-env -iA nixpkgs.ripgrep
 # clone vim and tmux setup
 git clone https://github.com/Yousuf28/ds_nix.git ~/ds_nix_config
 if [ -f .vimrc ]; then
