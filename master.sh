fc7bde243b495345e837321232de13195140cb8e
Yousuf Ali <yousuf.pharma@gmail.com>
2023-12-06 17:04:09 -0500

    glibc

--- master.sh
+++ master.sh
@@ -8,6 +8,7 @@ nix-env -iA tmux -f https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52
 nix-env -iA nodejs_20 -f https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz
 nix-env -iA python310Packages.python -f https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz
 nix-env -iA R -f https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz
+nix-env -iA nixpkgs.glibc
 nix-env -iA nixpkgs.rPackages.xml2
 nix-env -iA nixpkgs.rPackages.languageserver
 nix-env -iA nixpkgs.python310Packages.radian
@@ -17,6 +18,7 @@ nix-env -iA nixpkgs.fd
 nix-env -iA nixpkgs.ripgrep
 # clone vim and tmux setup
 rm -rf ds_nix_config/
+echo 'clone the repo '
 git clone https://github.com/Yousuf28/ds_nix.git ~/ds_nix_config
 if [ -f .vimrc ]; then
    mv .vimrc .vimrc.bak
@@ -33,7 +35,9 @@ cp ~/ds_nix_config/vimrc ~/.vimrc
 cp ~/ds_nix_config/tmux.conf ~/.tmux.conf
 cp ~/ds_nix_config/rprofile ~/.Rprofile
 # vim plugins manager (:PlugInstall) to install
+#
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# prefix and :source-file ~/.tmux.conf
 # tmux plugin manager (prefix + I) to install 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # font 
