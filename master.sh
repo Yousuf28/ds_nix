bbe798b17ffec0baccb42a7b01f4bc3cd49b872b
Yousuf Ali <yousuf.pharma@gmail.com>
2023-12-06 20:44:29 -0500

    update

--- master.sh
+++ master.sh
@@ -7,12 +7,8 @@ nix-env -iA vim -f https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52f
 nix-env -iA tmux -f https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz
 nix-env -iA nodejs_20 -f https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz
 nix-env -iA python310Packages.python -f https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz
-nix-env -iA R -f https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz
-nix-env -iA nixpkgs.glibc
-nix-env -iA nixpkgs.rPackages.xml2
-nix-env -iA nixpkgs.rPackages.languageserver
+# nix-env -iA R -f https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz
 nix-env -iA nixpkgs.python310Packages.radian
-
 nix-env -iA nixpkgs.fzf
 nix-env -iA nixpkgs.fd
 nix-env -iA nixpkgs.ripgrep
@@ -41,5 +37,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 # tmux plugin manager (prefix + I) to install 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # font 
-nix-env -iA nixpkgs.fira-code
-nix-env -iA nixpkgs.meslo-lg
+# nix-env -iA nixpkgs.fira-code
+# nix-env -iA nixpkgs.meslo-lg
--- r4.sh
+++ r4.sh
@@ -1,56 +1,11 @@
-apt-get update
-apt-get remove r-base-core -y
+#apt-get remove r-base-core -y
 
+apt-get update && apt-get install -y --no-install-recommends \
+	libxml2-dev
 
 R_VERSION_MAJOR=4
 R_VERSION_MINOR=3
 R_VERSION_PATCH=1
-
-CONFIGURE_OPTIONS="--with-cairo --with-jpeglib --enable-R-shlib --with-blas --with-lapack"
-CRAN_MIRROR=$CRAN_MIRROR
-apt-get update && apt-get install -y --no-install-recommends \
-    apt-utils\
-    gfortran \
-    # git \
-    g++ \
-    libreadline-dev \
-    libx11-dev \
-    libxt-dev \
-    libpng-dev \
-    libjpeg-dev \
-    libcairo2-dev \
-    libcurl4-openssl-dev \
-    libssl-dev \
-    libxml2-dev \
-    libudunits2-dev \
-    libgdal-dev \
-    libbz2-dev \
-    libzstd-dev \
-    liblzma-dev \
-    libpcre2-dev \
-    locales \
-    openjdk-8-jdk \
-    # screen \
-    texinfo \
-    # texlive \
-    # texlive-fonts-extra \
-    vim \
-    wget \
-    xvfb \
-    tzdata \
-    # sudo\
-    # jq\
-    curl\
-    libgit2-dev \
-    libmagick++-dev \
-    make \
-    tmux \
-    python3-launchpadlib \
-    python3.10-dev \
-    python3.10-venv \
-    python3-pip \
-    lsof \
-    && rm -rf /var/lib/apt/lists/*
 
 wget https://cran.rstudio.com/src/base/R-${R_VERSION_MAJOR}/R-${R_VERSION_MAJOR}.${R_VERSION_MINOR}.${R_VERSION_PATCH}.tar.gz
 
@@ -59,6 +14,6 @@ tar zxvf R-${R_VERSION_MAJOR}.${R_VERSION_MINOR}.${R_VERSION_PATCH}.tar.gz
 rm R-${R_VERSION_MAJOR}.${R_VERSION_MINOR}.${R_VERSION_PATCH}.tar.gz
 
 cd R-${R_VERSION_MAJOR}.${R_VERSION_MINOR}.${R_VERSION_PATCH}
-./configure ${CONFIGURE_OPTIONS} && \
+./configure --with-x=no && \
 	make && \
 	make install
