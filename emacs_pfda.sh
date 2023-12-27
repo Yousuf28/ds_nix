fb1011d1e1f606ed3e91b0c849e45ea256896994
Yousuf Ali <31291716+Yousuf28@users.noreply.github.com>
2023-12-27 11:56:48 -0500

    Update emacs_pfda.sh

--- emacs_pfda.sh
+++ emacs_pfda.sh
@@ -9,6 +9,6 @@ nix-env -iA nixpkgs.ripgrep
 nix-env -iA nixpkgs.emacs29-nox
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom install
-
+# https://github.com/doomemacs/doomemacs/issues/7292
 rm -r .emacs.d/
 
